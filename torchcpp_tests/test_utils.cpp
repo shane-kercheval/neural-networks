80a6609d98528ef1bd8f28552c7c0df91b40ce5a
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-04-21 14:42:45 -0700

    Update test_utils.cpp

--- torchcpp_tests/test_utils.cpp
+++ torchcpp_tests/test_utils.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "torchcpp/torchcpp.h"
+#include <Eigen/Dense>
 
 
 // Testing He initialization scale
@@ -14,7 +15,7 @@ TEST(UtilsTest, glorot_init_scale) {
 }
 
 // Testing MNIST data loading
-TEST(UtilsTest, load_mnist_data) {
+TEST(load_mnist_data, data_loads_successfully) {
     std::vector<Eigen::VectorXd> images;
     std::vector<int> labels;
     torchcpp_data::load_mnist_data(
@@ -27,8 +28,49 @@ TEST(UtilsTest, load_mnist_data) {
     EXPECT_EQ(images.size(), 1000);
     EXPECT_EQ(labels.size(), 1000);
     EXPECT_EQ(images[0].size(), 28 * 28);
-    EXPECT_EQ(labels[0], 5);
+    EXPECT_EQ(labels[0], 5);  // First label in the MNIST dataset
+    EXPECT_EQ(labels[1], 0);  // Second label in the MNIST dataset
 }
+
+TEST(load_mnist_data, fails_on_invalid_path) {
+    std::vector<Eigen::VectorXd> images;
+    std::vector<int> labels;
+    EXPECT_THROW(
+        torchcpp_data::load_mnist_data(
+            images,
+            labels,
+            "data/invalid-path",
+            "data/invalid-path",
+            1000
+        ),
+        std::runtime_error
+    );
+}   
+
+TEST(EigenOperationExample, ElementWiseOperation_add) {
+    Eigen::MatrixXd matrix_a(2, 2);
+    matrix_a << 1, 2,
+                3, 4;
+    Eigen::MatrixXd matrix_b(2, 2);
+    matrix_b << 5, 6,
+                7, 8;
+    Eigen::MatrixXd expected(2, 2);
+    expected << 6, 8,
+                10, 12;
+    Eigen::MatrixXd actual = matrix_a.array() + matrix_b.array();
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(EigenOperationExample, ElementWiseOperation_add_row_wise) {
+    Eigen::MatrixXd matrix_a(2, 2);
+    matrix_a << 1, 2,
+                3, 4;
+    Eigen::VectorXd expected(2);
+    expected << 4, 6;
+    Eigen::VectorXd actual = matrix_a.rowwise().sum();
+}
+
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
