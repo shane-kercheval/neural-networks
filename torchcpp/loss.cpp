9501d48a3b9eb7ac9319ec4571bb9d80ec313b9c
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-04-27 15:05:43 -0700

    update loss.cpp based on linting warnings

--- torchcpp/loss.cpp
+++ torchcpp/loss.cpp
@@ -10,29 +10,30 @@ double CrossEntropyLoss::forward(const MatrixXd& logits, const VectorXi& targets
     }
     Eigen::MatrixXd softmax_probs = softmax(logits);
 
-    const int num_examples = logits.rows();
+    const long num_examples = logits.rows();
     double loss = 0.0;
-    for (int i = 0; i < num_examples; ++i) {
+    const double clipped = 1e-15;
+    for (long i = 0; i < num_examples; ++i) {
         double prob = softmax_probs(i, targets(i));
-        loss += -std::log(std::max(prob, 1e-15));
+        loss += -std::log(std::max(prob, clipped));
     }
-    return loss / num_examples;
+    return loss / static_cast<double>(num_examples);
 }
 
 Eigen::MatrixXd CrossEntropyLoss::backward() {
     if (!Module::training) {
         throw std::logic_error("Module::backward should only be called during training");
     }
-    const int num_examples = logits_cache_.rows();
+    const long num_examples = logits_cache_.rows();
     if (num_examples == 0) {
         throw std::logic_error("No data in cache. Make sure to call forward first.");
     }
     Eigen::MatrixXd softmax_probs = softmax(logits_cache_);
     Eigen::MatrixXd grad_logits = softmax_probs;
-    for (int i = 0; i < targets_cache_.size(); ++i) {
+    for (long i = 0; i < targets_cache_.size(); ++i) {
         grad_logits(i, targets_cache_(i)) -= 1;
     }
-    grad_logits /= targets_cache_.size();
+    grad_logits /= static_cast<double>(targets_cache_.size());
     return grad_logits;
 }
 
