122e21e6bfcb526ea45bbae8958e5f70f3a0e73f
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-04-27 11:30:32 -0700

    Update activations.h

--- torchcpp/activations.h
+++ torchcpp/activations.h
@@ -12,12 +12,29 @@ public:
 
     /**
      * @brief Forward pass of the ReLU activation function.
+     * 
+     * @param x Input data.
     */
     MatrixXd forward(const MatrixXd& x) override;
 
 protected:
     /**
-     * @brief Backward pass of the ReLU activation function.
+     * @brief Backward pass of ReLU.
+     * 
+     * The ReLU function is defined as:
+     *      `f(x) = max(0, x)`
+     * 
+     * The derivative of ReLU:
+     *  `f'(x) = 1 if x > 0 else 0`
+     * 
+     * Therefore:
+     *      `∂L/∂x = ∂L/∂f * ∂f/∂x`, where
+     *          - `∂f/∂x` = f'(x)
+     *          - `∂L/∂f` is the gradient of the loss function with respect to the output of this
+     *             layer, which is passed as grad_output.
+     * 
+     * @param grad_output (∂L/∂y) Gradient of the loss with respect to the output of this layer.
+     * Calculated in the next layer and passed to this layer during backpropagation.
     */
     MatrixXd backward_impl(const MatrixXd& grad_output) override;
 
